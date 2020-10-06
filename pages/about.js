d603de0bcb4a22b993af05ca074fe8089627d173
Nguyen <ntrungnguyen845@gmail.com>
2020-10-06 19:47:16 +0700

    firt-project

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+about.propTypes = {
+    
+};
+
+function about(props) {
+    return (
+        <div>
+            <p>Hello about</p>
+        </div>
+    );
+}
+
+export default about;
+
+///helloqweqeq
